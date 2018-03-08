663536359c5b0ef7457e3a15abcd786bd47bb101
franco.fral <franck_ral@yahoo.com>
2018-03-08 12:30:37 -0400

    ionic-services-alerts: Working on alerts and back to home page.

--- src/pages/add/add.component.ts
+++ src/pages/add/add.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from 'ionic-angular';
 
 import { List, ListItem } from '../../app/classes/index';
+import { WishListService } from '../../app/services/wish-list.service';
 
 @Component({
   selector: 'app-add',
@@ -8,12 +10,15 @@ import { List, ListItem } from '../../app/classes/index';
 })
 export class AddComponent implements OnInit {
 
-  listName:string;
+  listName:string = "";
   itemName:string = "";
 
   items: ListItem[] = [];
 
-  constructor() {  }
+  constructor(public alertController: AlertController,
+              public navControler: NavController,
+              public _wishService: WishListService) {
+  }
 
   ngOnInit() {}
 
@@ -33,7 +38,27 @@ export class AddComponent implements OnInit {
     if(index < 0) {
       return;
     }
-    
+
     this.items.splice(index, 1);
+  }
+
+  saveList() {
+    if (this.listName.length == 0) {
+      let alert = this.alertController.create({
+        title: 'List Name',
+        subTitle: 'The List Name is required.',
+        buttons: ['OK']
+      });
+
+      alert.present();
+      return;
+    }
+
+    let list = new List(this.listName);
+    list.items = this.items;
+
+    this._wishService.lists.push(list);
+
+    this.navControler.pop();
   }
 }
